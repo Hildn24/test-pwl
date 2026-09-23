eff0bcae257ace2635cc092f4ac79bfd1c93446c
Muhammad Hildan Alfaris <hildanalfaris@gmail.com>
2026-09-23 08:06:29 +0700

    inisialisasi project laravel

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <tr>
+        <td>Nama</td>
+        <td>{{ $nama }}</td>
+    </tr>
+    <tr>
+        <td>NPM</td>
+        <td>{{ $npm }}</td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>{{ $kelas }}</td>
+    </tr>
+</body>
+</html>
